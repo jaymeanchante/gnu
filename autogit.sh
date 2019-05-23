1724ba8181dded5952f181b5bb58d290d92af0b0
Jayme Anchante <jayme.anchante@disroot.org>
2019-05-23 08:49:22 -0300

    updated autogit.sh * separating repos by login * fetching all content

--- autogit.sh
+++ autogit.sh
@@ -10,12 +10,27 @@ git clone "https://$username:$password@github.com/jayme-anchante/repos"
 
 while read p
 do
-	git clone "https://$username:$password@github.com/jayme-anchante/$p"
+    dir=$(echo $p | cut -d "/" -f 2)
+    repo=$(echo $p | cut -d "/" -f 3)
+    mkdir -p $dir
+    cd $dir
+    git clone "http://"$username":"$password$p
+    cd $repo
+    git fetch --all
+    cd ../../
 done < repos/private.txt
 
 while read p
 do
 	git clone "https://github.com/jayme-anchante/$p"
+    dir=$(echo $p | cut -d "/" -f 2)
+    repo=$(echo $p | cut -d "/" -f 3)
+    mkdir -p $dir
+    cd $dir
+    git clone "http://"$p
+    cd $repo
+    git fetch --all
+    cd ../../
 done < repos/public.txt
 
 echo "creating a git-credentials file in ~/.git-credentials
