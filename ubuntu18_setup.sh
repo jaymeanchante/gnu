395f45a00e75e5cabf31bb436ec3716c1e58743b
Jayme Anchante <jayme.anchante@disroot.org>
2020-04-20 13:33:14 -0300

    :penguin: adding latest nodejs LTS dependency

--- ubuntu18_setup.sh
+++ ubuntu18_setup.sh
@@ -93,3 +93,11 @@ sudo apt-get install -y gimp
 
 # v4l2loopback - a kernel module for creating virtual video devices
 sudo apt-get install -y v4l2loopback-dkms v4l2loopback-utils
+
+# latest node LTS
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# optional: install yarn
+# curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# sudo apt-get update && sudo apt-get install yarn
