ff1ad735b4d401d9763928b198eaf421d9c20e60
Jayme Anchante <jayme.anchante@disroot.org>
2020-04-28 19:48:39 -0300

    :penguin: Adding yarn install

--- ubuntu18_setup.sh
+++ ubuntu18_setup.sh
@@ -97,7 +97,6 @@ sudo apt-get install -y v4l2loopback-dkms v4l2loopback-utils
 # latest node LTS
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
-# optional: install yarn
-# curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-# sudo apt-get update && sudo apt-get install yarn
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
