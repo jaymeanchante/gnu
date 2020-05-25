a71d235cc5360e70824259cecf4b147f20a9148d
Jayme Anchante <jayme.anchante@disroot.org>
2020-05-25 20:06:36 -0300

    :penguin: added latex to ubuntu18_setup.sh

--- ubuntu18_setup.sh
+++ ubuntu18_setup.sh
@@ -100,3 +100,6 @@ sudo apt-get install -y nodejs
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
+
+# latex
+sudo apt-get install -y impressive pandoc texlive-latex-recommended
