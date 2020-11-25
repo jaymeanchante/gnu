0c220ad6e10d0d088781dc072214e479c395d9cc
Jayme Anchante <jayme.anchante@disroot.org>
2020-11-25 11:54:20 -0300

    new packages

--- os_setups/pp_arch.sh
+++ os_setups/pp_arch.sh
@@ -4,10 +4,13 @@
 sudo pacman -Syu
 
 # utils
-sudo pacman -S git wget htop neofetch
+sudo pacman -S git wget htop neofetch unzip
 
 # android apps
 sudo pacman -S anbox anbox-image-aarch64 adb
 
 # nice apps
 sudo pacman -S foliate
+
+# comms
+sudo pacman -S telegram-desktop
