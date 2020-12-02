08802eec9f2fef57c4b08a570e10b888433bf009
Jayme Anchante <jayme.anchante@disroot.org>
2020-12-02 10:43:17 -0300

    :sparkles: added some packages to pinephone arch installation

--- os_setups/pp_arch.sh
+++ os_setups/pp_arch.sh
@@ -3,14 +3,44 @@
 # system wide update
 sudo pacman -Syu
 
+# yay
+sudo pacman -S fakeroot
+cd /tmp
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+
+
 # utils
-sudo pacman -S git wget htop neofetch unzip
+sudo pacman -S \
+	git \
+	wget \
+	htop \
+	neofetch \
+	unzip \
+	make \
+	cmake \
+	gcc \
+	sdl2 \
+	sdl2_mixer \
+	libpng \
+	wlr-randr \ # wlr-randr --output DSI-1 --off
 
 # android apps
 sudo pacman -S anbox anbox-image-aarch64 adb
 
 # nice apps
-sudo pacman -S foliate
+sudo pacman -S \
+	foliate \
+	libreoffice \
+	newsboat
 
 # comms
 sudo pacman -S telegram-desktop
+
+# music
+sudo pacman -S mpv mps-youtube
+
+# setup
+sudo systemctl start sshd.service
+sudo systemctl enable sshd.service
